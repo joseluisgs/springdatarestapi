f0c739e3cbe4abc571fe5aae477ad5fc6e06d093
joseluisgs <joseluisgonsan@gmail.com>
2020-03-24 13:06:29 +0100

    SpringDataRest 3 Busqueda y Paginación

--- src/main/java/com/joseluisgs/springdatarestapi/repositorios/CiudadRepositorio.java
+++ src/main/java/com/joseluisgs/springdatarestapi/repositorios/CiudadRepositorio.java
@@ -1,11 +1,22 @@
 package com.joseluisgs.springdatarestapi.repositorios;
 
 import com.joseluisgs.springdatarestapi.modelos.Ciudad;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 // Nos indica el path automaticamente desde el path donde se va a exponer
 @RepositoryRestResource(path = "ciudades", collectionResourceRel = "ciudades")
 public interface CiudadRepositorio extends JpaRepository<Ciudad, Long> {
 
+    // Consulta personalizada
+    @RestResource(path = "nombreComienzaPor", rel = "nombreComienzaPor")
+    public Page<Ciudad> findByNombreStartsWith(@Param("nombre") String nombre, Pageable p);
+
+    // Consulta personalizada
+    @RestResource(path = "nombreContieneA", rel = "nombreContieneA")
+    public Page<Ciudad> findByNombreContaining(@Param("nombre") String nombre, Pageable p);
 }
--- src/main/java/com/joseluisgs/springdatarestapi/repositorios/PaisRepositorio.java
+++ src/main/java/com/joseluisgs/springdatarestapi/repositorios/PaisRepositorio.java
@@ -1,12 +1,25 @@
 package com.joseluisgs.springdatarestapi.repositorios;
 
+import com.joseluisgs.springdatarestapi.modelos.Ciudad;
 import com.joseluisgs.springdatarestapi.modelos.Pais;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 // Punto de entrada
 @RepositoryRestResource(path = "paises", collectionResourceRel = "paises")
 //@RepositoryRestResource(exported = false) // Activa o desactiva que el repositorio se exporte
 public interface PaisRepositorio extends JpaRepository<Pais, Long> {
+
+    // Consulta personalizada
+    @RestResource(path = "nombreComienzaPor", rel = "nombreComienzaPor")
+    public Page<Pais> findByNombreStartsWith(@Param("nombre") String nombre, Pageable p);
+
+    // Consulta personalizada
+    @RestResource(path = "nombreContieneA", rel = "nombreContieneA")
+    public Page<Pais> findByNombreContaining(@Param("nombre") String nombre, Pageable p);
 
 }
